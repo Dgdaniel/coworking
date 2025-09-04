aef69c52c543706bef767a8575f8f207d6a67e28
justinadjassem <justin.adjassem@gmail.com>
2025-09-04 16:58:20 +0000

    project init

---
+++ src/test/java/net/devpull/coworking/ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.devpull.coworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
